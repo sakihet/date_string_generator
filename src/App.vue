e895121c81f5e24f4ea4558a144888c2db811e97
saki <sakihet@gmail.com>
2017-11-12 21:33:33 +0900

    implement

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <DateStringGenerator/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import DateStringGenerator from './components/DateStringGenerator'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DateStringGenerator
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/DateStringGenerator.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <label>prefix:
+      <input type="text" v-model="prefix">
+    </label>
+    <br>
+    <label>locale:
+      <input type="text" v-model="locale">
+    </label>
+    <br>
+    <label>days:
+      <input type="number" v-model="days">
+    </label>
+    <br>
+    <textarea v-model="textarea" rows="20" style="font-family: monospace;"></textarea>
+    <br>
+    <button @click="copy">copy to clipboard</button>
+  </div>
+</template>
+
+<script>
+const copy = require('copy-text-to-clipboard')
+const moment = require('moment')
+
+export default {
+  name: 'DateStringGenerator',
+  data () {
+    return {
+      msg: 'date string generator',
+      prefix: '# ',
+      text: 'test',
+      locale: 'en',
+      days: 7
+    }
+  },
+  computed: {
+    textarea () {
+      moment.locale(this.locale)
+      var str = ''
+      for (var i = 0; i < this.days; i++) {
+        str += this.prefix + moment().add(i, 'day').format('YYMMDDddd')
+        str += '\n'
+      }
+      return str
+    }
+  },
+  methods: {
+    copy () {
+      copy(this.textarea)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
